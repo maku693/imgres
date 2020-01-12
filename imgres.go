7a7812e58f89aa98285305b4ea603f314314093d
maku693 <me@shotahamada.com>
2020-01-12 16:37:24 +0900

    Sort flags alphabetically

--- imgres.go
+++ imgres.go
@@ -7,14 +7,14 @@ import (
 	"os"
 )
 
+var height, width int
 var in, out string
-var width, height int
 
 func init() {
 	flag.StringVar(&in, "in", "", "input file (optional)")
 	flag.StringVar(&out, "out", "", "out file (optional)")
-	flag.IntVar(&width, "width", 0, "max width of out file")
 	flag.IntVar(&height, "height", 0, "max height of out file")
+	flag.IntVar(&width, "width", 0, "max width of out file")
 }
 
 func main() {
